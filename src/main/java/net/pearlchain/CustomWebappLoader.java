d7ba92173806d2dea107844e4caafe9d0ed78f5d
SergeyVorozhtsov <sergey.vorozhtsov@pearlchain.net>
2018-10-15 17:17:30 +0700

    fixes

--- src/main/java/net/pearlchain/CustomWebappLoader.java
+++ src/main/java/net/pearlchain/CustomWebappLoader.java
@@ -3,22 +3,13 @@ package net.pearlchain;
 import org.apache.catalina.loader.WebappLoader;
 
 public class CustomWebappLoader extends WebappLoader {
-    static
-    {
-        System.out.println("CustomWebappLoader static init");
-        ClassLoader classLoader = new CustomWebappClassLoader();
-    }
-
-
     public CustomWebappLoader() {
         super();
-        super.setLoaderClass("CustomWebappClassLoader");
-        System.err.println("CustomWebappLoader constructor" + this);
+        super.setLoaderClass("net.pearlchain.CustomWebappClassLoader");
     }
 
     public CustomWebappLoader(ClassLoader parent) {
         super(parent);
-        super.setLoaderClass("CustomWebappClassLoader");
-        System.err.println("CustomWebappLoader constructor + " + this + " " + parent);
+        super.setLoaderClass("net.pearlchain.CustomWebappClassLoader");
     }
 }
